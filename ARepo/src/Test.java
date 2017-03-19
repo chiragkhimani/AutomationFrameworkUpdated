beafece4c9f9d7b9699edaa25369a9b3ae7d1b9d
Chirag <khimanichirag@gmail.com>
2017-03-19 23:38:20 +0530

    Feature added

--- ARepo/src/Test.java
+++ ARepo/src/Test.java
@@ -1,4 +1,9 @@
 
 public class Test {
-
+	public static void main(String[] args) {
+		System.out.println("Feature1 Acc");
+		System.out.println("Feature2 Acc");
+		System.out.println("Feature3 Acc");
+		System.out.println("Feature4 Acc");
+	}
 }
